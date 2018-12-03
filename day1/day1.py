e6962b8f694163b7b2633461c42eaaca1abb3329
Karl Siil <karliansiil@gmail.com>
2018-12-02 19:21:11 -0600

    added solutions to day1

---
+++ day1/day1.py
@@ -0,0 +1,22 @@
+def read(path):
+    with open(path) as file:
+        numbers = [int(x) for x in file.readlines()]
+    return numbers
+
+def recalibrate(input_list):
+    return sum(input_list)
+
+def loop(input_list):
+    ans = []
+    total = 0
+    while total not in ans:
+        for value in input_list:
+            ans.append(total)
+            total += value
+            if total in ans:
+                return total
+    
+
+if __name__ == "__main__":    
+    print('Part1: ', recalibrate(read('day1_input.txt')))
+    print('Part2: ', loop(read('day1_input.txt')))
